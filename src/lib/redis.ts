fd960e4d02f49e258182a32b4c9fda78306d7836
bluejutzu <kalebdaniel98@gmail.com>
2024-03-09 13:55:10 +0100

    Removed redis.ts

--- src/lib/redis.ts
+++
@@ -1,8 +0,0 @@
-/** @format */
-
-import { Redis } from "@upstash/redis";
-
-export const redis = new Redis({
-  url: "https://us1-wondrous-python-40103.upstash.io",
-  token: process.env.REDIS_KEY!,
-});
