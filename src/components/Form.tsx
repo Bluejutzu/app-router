71fb7f3807542f3e4b8ec314f345cf22148fed61
bluejutzu <kalebdaniel98@gmail.com>
2024-03-14 19:28:02 +0100

    Test

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,7 +6,7 @@ dotenv.config();
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -19,6 +19,10 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { toast } from "@/components/ui/use-toast";
+ import {
+  EmbedBuilder,
+  
+} from "discord.js"; 
 
 const FormSchema = z.object({
   suggestion: z.string().min(10, {
@@ -46,31 +50,36 @@ export function InputForm() {
       });
       const suggestion = JSON.stringify(data);
 
-      const params = {
-        content: `API test via https://bluejutzu.vercel.app/ \n Suggestion JSON: ${suggestion}`,
-        username: "Send by API"
-      };
+       const token =
+         "6yHjJbFteB7AlzjrSyYtZGusNv8ykqo7Cx0oELkpJPGZW7lvyN3pr46tqYYxj-Z2wNaa";
+       const url = `https://discord.com/api/v10/webhooks/1216394063721529434/${token}`;
+       
 
-      const token =
-        "6yHjJbFteB7AlzjrSyYtZGusNv8ykqo7Cx0oELkpJPGZW7lvyN3pr46tqYYxj-Z2wNaa";
-      const headers = {
-        "Content-Type": "application/json",
-      };
+       const suggestionEmbed = new EmbedBuilder()
+         .setAuthor({ name: "Suggestion System" })
+         .setTitle(`New Suggestion submitted`)
+         .setDescription(`From`)
+         .setFields({
+           name: "Suggestion:",
+           value: "Mock text",
+         })
+         .setTimestamp();
 
-      const req = await axios
-        .post(
-          `https://discord.com/api/v10/webhooks/1216394063721529434/${token}`,
-          params,
-          {
-            headers,
-          }
-        )
-        .then((response: AxiosResponse<Response>) => {
-          console.log(response.data);
-        })
-        .catch((error: any) => {
-          console.error("An error occured:", error);
-        });
+       const params = {
+         embeds: [suggestionEmbed],
+         username: "Send by API",
+       };
+       const headers = {
+         "Content-Type": "application/json",
+       };
+
+       const req = await axios
+         .post(url, params, {
+           headers,
+         })
+         .catch((error) => {
+           console.error("An error occured:", error);
+         });
     } catch (error) {}
     console.log(
       `You submitted the following values: \n ${JSON.stringify(data)}`
@@ -81,7 +90,7 @@ export function InputForm() {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className='mx-auto w-2/3 space-y-4 text-white max-w-lg bg-slate-400 p-5 rounded-2xl '>
+        className='mx-auto  space-y-4 text-white max-w-lg bg-slate-400 p-5 rounded-2xl '>
         <FormField
           control={form.control}
           name='suggestion'
