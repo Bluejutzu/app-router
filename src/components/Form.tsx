5616308ec75666e5860928dd665f2e08b40d8328
bluejutzu <kalebdaniel98@gmail.com>
2024-03-15 13:37:06 +0100

    Finished webhook execution and suggestion system

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -18,12 +18,15 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { toast } from "@/components/ui/use-toast";
-import { suggestionEmbed } from "./utils/suggestionEmbed";
+import ModalWrapper from "./Attention";
+const websiteUrl = "https://bluejutzu.vercel.app/";
 
 const FormSchema = z.object({
-  suggestion: z.string().min(10, {
-    message: "Suggestion must be at least 10 characters.",
+  username: z.string().min(1, {
+    message: "Username must be at least 1 character.",
+  }),
+  suggestion: z.string().min(5, {
+    message: "Suggestion must be at least 5 characters.",
   }),
 });
 
@@ -31,42 +34,38 @@ export function InputForm() {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
+      username: "",
       suggestion: "",
     },
   });
 
   async function onSubmit(data: z.infer<typeof FormSchema>) {
     try {
-      toast({
-        title: "You submitted the following values:",
-        description: (
-          <pre className='mt-2 w-[340px] rounded-md bg-slate-950 p-4'>
-            <code className='text-white'>{JSON.stringify(data, null, 2)}</code>
-          </pre>
-        ),
-      });
-      const suggestion = JSON.stringify(data);
+      const username = data.username;
+      const suggestion = data.suggestion;
 
-       const token =
-         "6yHjJbFteB7AlzjrSyYtZGusNv8ykqo7Cx0oELkpJPGZW7lvyN3pr46tqYYxj-Z2wNaa";
-       const url = `https://discord.com/api/v10/webhooks/1216394063721529434/${token}`;
+      const token =
+        "6yHjJbFteB7AlzjrSyYtZGusNv8ykqo7Cx0oELkpJPGZW7lvyN3pr46tqYYxj-Z2wNaa";
+      const url = `https://discord.com/api/v10/webhooks/1216394063721529434/${token}`;
 
-       const params = {
-         embeds: [suggestionEmbed],
-         username: "Send by API",
-       };
-       const headers = {
-         "Content-Type": "application/json",
-       };
+      const params = {
+        username: username,
+        content: suggestion,
+      };
+      const headers = {
+        "Content-Type": "application/json",
+      };
 
-       const req = await axios
-         .post(url, params, {
-           headers,
-         })
-         .catch((error) => {
-           console.error("An error occured:", error);
-         });
-    } catch (error) {}
+      const req = await axios
+        .post(url, params, {
+          headers,
+        })
+        .catch((error) => {
+          console.error("An error occured:", error);
+        });
+    } catch (error) {
+      console.log(error);
+    }
     console.log(
       `You submitted the following values: \n ${JSON.stringify(data)}`
     );
@@ -76,7 +75,25 @@ export function InputForm() {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className='mx-auto  space-y-4 text-white max-w-lg bg-slate-400 p-5 rounded-2xl '>
+        className='mx-auto  space-y-4 text-white max-w-lg bg-gradient-to-r from-[#252a3f] to-[#423555] p-7 rounded-2xl drop-shadow-2xl '>
+        <FormField
+          control={form.control}
+          name='username'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input
+                  className='text-gray-900 '
+                  placeholder='Max Mustermann'
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>Username to display</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
         <FormField
           control={form.control}
           name='suggestion'
@@ -97,7 +114,8 @@ export function InputForm() {
             </FormItem>
           )}
         />
-        <Button type='submit'>Submit</Button>
+
+        <ModalWrapper />
       </form>
     </Form>
   );
